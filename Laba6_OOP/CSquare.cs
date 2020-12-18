ed18dcea6f183187a357070be427968d6cf2d597
Khavanova Olga <khavanova@mail.ru>
2020-12-18 20:53:51 +0500

    Изменение 7. Создание класса СSquare.

--- Laba6_OOP/CSquare.cs
+++ Laba6_OOP/CSquare.cs
@@ -2,11 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Threading.Tasks;
 
 namespace Laba6_OOP
 {
-    class CSquare
+    public class CSquare : СRectangle
     {
+        public CSquare(int x, int y) : base(x, y)
+        {
+
+        }
+
+        protected override void makePath()
+        {
+            path.Reset();
+            path.AddRectangle(new RectangleF((x - ((height * 2) / 2)), (y - (height / 2)), height, height));
+            boundsRec = path.GetBounds();
+        }
     }
 }
