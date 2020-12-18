e96dcf8bac267de72efd9e233f3013fef36a0e3a
Khavanova Olga <khavanova@mail.ru>
2020-12-18 20:17:21 +0500

    Изменение 4. Вставка класса ССircle из ЛР4.

---
+++ Laba6_OOP/CCircle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Threading.Tasks;
+
+namespace Laba6_OOP
+{
+    class CCircle : Shape
+    {
+        public int radius;
+        public int minRadius;
+
+
+        public CCircle(int x, int y)
+        {
+            minRadius = 1;
+            radius = 20;
+            this.x = x;
+            this.y = y;
+            path = new GraphicsPath();
+            lastCommand = "Constructor";
+        }
+
+        public override void scale(int delta)
+        {
+            radius += delta;
+            if (radius < minRadius)
+            {
+                radius = minRadius;
+            }
+
+            lastCommand = "scale";
+        }
+
+        protected override void makePath()
+        {
+            path.Reset();
+            path.AddEllipse(x - radius, y - radius, radius * 2, radius * 2);
+            boundsRec = path.GetBounds();
+        }
+    }
+}
