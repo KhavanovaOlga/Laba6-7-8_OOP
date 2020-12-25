8aa800dad940fbd67b6dce7890c24d1a710afb93
Khavanova Olga <khavanova@mail.ru>
2020-12-25 13:12:55 +0500

    Изменение 13. Создание класса Observer.cs.

---
+++ Laba6_OOP/Observer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba6_OOP
+{
+    public interface IObserver
+    {
+        void onSubjectChanged(ISubject subject);
+    }
+
+    public interface ISubject
+    {
+        void addObserver(IObserver observer);
+
+        void removeObserver(IObserver observer);
+
+        void notifyEveryone();
+    }
+}
